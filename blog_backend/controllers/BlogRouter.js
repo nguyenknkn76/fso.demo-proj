3a60f84c8b8f49d2dc444bb57fae034fe7d08c89
Nguyen <nguyenknkn76@gmail.com>
2024-04-28 01:22:04 +0700

    use asyns await instead of promises

--- blog_backend/controllers/BlogRouter.js
+++ blog_backend/controllers/BlogRouter.js
@@ -1,22 +1,15 @@
 const blogRouter = require('express').Router()
 const Blog = require("../models/BlogModel")
 
-blogRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+blogRouter.get('/',  async (request, response, next) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
   })
   
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  await blog.save()
+  response.status(201).json(blog)
 })
 
 module.exports = blogRouter
