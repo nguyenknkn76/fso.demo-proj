41fea600b8105d022dd01481d35feee90a24da40
Khoi Nguyen <nguyenknkn76@gmail.com>
2025-07-10 01:01:43 +0700

    Revert "check"
    This reverts commit 1b2bca1accba708a4928090c6fa1d46581cba512.

---
+++ ts-app/backend/index.ts
@@ -0,0 +1,30 @@
+// const express = require('express');
+import express from 'express';
+// import {calculator2} from './calculate.ts'
+// import _ from 'lodash';
+import { calculator2, Operation } from './calculate';
+
+const app = express();
+
+app.get('/ping', (_req, res) => {
+  res.send('pong');
+});
+
+app.post('/calculate', (req, res) => {
+  const {value1, value2, op} = req.body; /* no clue what the type will be */
+
+  if ( !value1 || isNaN(Number(value1))) {
+    res.status(400).send({ error: '...'});
+  }
+
+  const operation = op as Operation;
+  const result  = calculator2(Number(value1), Number(value2), operation); 
+  res.json({result});
+});
+
+const PORT = 3000;
+app.listen(PORT , () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log('To test the server, visit http://localhost:3000/ping'
+  );
+});
